eec60fcf417ff4d73b17ac0b5ef24e27e9bbb5eb
shushovitJR <ranashushovit@gmail.com>
2025-11-11 20:04:54 +0545

    Made a header file

--- E-Commerce project/src/pages/TrackingPage.jsx
+++ E-Commerce project/src/pages/TrackingPage.jsx
@@ -1,77 +1,44 @@
-import './TrackingPage.css';
+import { Link } from "react-router";
+import { Header } from "../components/Header";
+import "./TrackingPage.css";
 
-export function TrackingPage(){
-    return(
-        <>
-            <div className="header">
-      <div className="left-section">
-        <a href="/" className="header-link">
-          <img className="logo"
-            src="images/logo-white.png" />
-          <img className="mobile-logo"
-            src="images/mobile-logo-white.png" />
-        </a>
-      </div>
+export function TrackingPage() {
+  return (
+    <>
+      <title>Tracking</title>
 
-      <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+      <Header />
 
-        <button className="search-button">
-          <img className="search-icon" src="images/icons/search-icon.png" />
-        </button>
-      </div>
+      <div className="tracking-page">
+        <div className="order-tracking">
+          <Link className="back-to-orders-link link-primary" to="/orders">
+            View all orders
+          </Link>
 
-      <div className="right-section">
-        <a className="orders-link header-link" href="/orders">
+          <div className="delivery-date">Arriving on Monday, June 13</div>
 
-          <span className="orders-text">Orders</span>
-        </a>
-
-        <a className="cart-link header-link" href="/checkout">
-          <img className="cart-icon" src="images/icons/cart-icon.png" />
-          <div className="cart-quantity">3</div>
-          <div className="cart-text">Cart</div>
-        </a>
-      </div>
-    </div>
-
-    <div className="tracking-page">
-      <div className="order-tracking">
-        <a className="back-to-orders-link link-primary" href="/orders">
-          View all orders
-        </a>
-
-        <div className="delivery-date">
-          Arriving on Monday, June 13
-        </div>
-
-        <div className="product-info">
-          Black and Gray Athletic Cotton Socks - 6 Pairs
-        </div>
-
-        <div className="product-info">
-          Quantity: 1
-        </div>
-
-        <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
-
-        <div className="progress-labels-container">
-          <div className="progress-label">
-            Preparing
+          <div className="product-info">
+            Black and Gray Athletic Cotton Socks - 6 Pairs
           </div>
-          <div className="progress-label current-status">
-            Shipped
+
+          <div className="product-info">Quantity: 1</div>
+
+          <img
+            className="product-image"
+            src="images/products/athletic-cotton-socks-6-pairs.jpg"
+          />
+
+          <div className="progress-labels-container">
+            <div className="progress-label">Preparing</div>
+            <div className="progress-label current-status">Shipped</div>
+            <div className="progress-label">Delivered</div>
           </div>
-          <div className="progress-label">
-            Delivered
+
+          <div className="progress-bar-container">
+            <div className="progress-bar"></div>
           </div>
         </div>
-
-        <div className="progress-bar-container">
-          <div className="progress-bar"></div>
-        </div>
       </div>
-    </div>
     </>
-    );
+  );
 }
