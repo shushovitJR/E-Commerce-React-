b997a7926fb8483ec916a832202f1143b4f6285a
shushovitJR <ranashushovit@gmail.com>
2025-11-12 11:12:54 +0545

    Updated checkout page

--- E-Commerce project/src/pages/HomePage.jsx
+++ E-Commerce project/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect,useState } from 'react';
 import { Header } from "../components/Header";
-// import { products } from "../../../Starting-code/data/products";
+import { formatMoney } from '../utils/money';
 import "./HomePage.css";
 
 export function HomePage({ cart }) {
@@ -45,7 +45,7 @@ export function HomePage({ cart }) {
                   <div className="product-rating-count link-primary">{product.rating.count}</div>
                 </div>
 
-                <div className="product-price">${(product.priceCents/100).toFixed(2)}</div>
+                <div className="product-price">{formatMoney(product.priceCents)}</div>
 
                 <div className="product-quantity-container">
                   <select>
