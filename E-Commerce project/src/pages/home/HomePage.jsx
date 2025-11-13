8338f4e0f67a3147d5cb11bb4c4146d47b7c70a4
shushovitJR <ranashushovit@gmail.com>
2025-11-13 19:06:44 +0545

    added async await to fetch data

--- E-Commerce project/src/pages/home/HomePage.jsx
+++ E-Commerce project/src/pages/home/HomePage.jsx
@@ -9,10 +9,12 @@ export function HomePage({ cart }) {
   
 
   useEffect(()=>{
-    axios.get('/api/products')
-    .then((response)=>{
-      setProducts(response.data);
-    });
+    const getHomeData = async () =>{
+      const response = await axios.get('/api/products');
+        setProducts(response.data);
+    };
+
+    getHomeData();
   },[]);
 
 
