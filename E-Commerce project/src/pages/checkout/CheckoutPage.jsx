b5172a761a62b69a4087b61fc0cc7ba5d7c8bcbb
shushovitJR <ranashushovit@gmail.com>
2025-11-14 12:03:15 +0545

    added function to change delivery option

--- E-Commerce project/src/pages/checkout/CheckoutPage.jsx
+++ E-Commerce project/src/pages/checkout/CheckoutPage.jsx
@@ -5,7 +5,7 @@ import "./CheckoutPage.css";
 import { OrderSummary } from "./OrderSummary";
 import { PaymentSummary } from "./PaymentSummary";
 
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
 
@@ -21,7 +21,7 @@ export function CheckoutPage({ cart }) {
     }
 
     fetchCheckoutData();
-  }, []);
+  }, [cart]);
 
   return (
     <>
@@ -55,7 +55,7 @@ export function CheckoutPage({ cart }) {
 
         <div className="checkout-grid">
 
-            <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+            <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart} />
 
             <PaymentSummary paymentSummary={paymentSummary} />
         </div>
--- E-Commerce project/src/pages/checkout/DeliveryOptions.jsx
+++ E-Commerce project/src/pages/checkout/DeliveryOptions.jsx
@@ -1,7 +1,8 @@
 import { formatMoney } from "../../utils/money";
+import axios from "axios";
 import dayjs from "dayjs";
 
-export function DeliveryOptions({ cartItem, deliveryOptions }) {
+export function DeliveryOptions({ cartItem, deliveryOptions, loadCart }) {
   return (
     <div className="delivery-options">
       <div className="delivery-options-title">Choose a delivery option:</div>
@@ -12,11 +13,20 @@ export function DeliveryOptions({ cartItem, deliveryOptions }) {
           priceString = `${formatMoney(deliveryOption.priceCents)} - Shipping`;
         }
 
+        const updateDeliveryOption = async () =>{
+          await axios.put(`/api/cart-items/${cartItem.productId}`, {
+            deliveryOptionId: deliveryOption.id
+          });
+          await loadCart();
+        }
+
         return (
-          <div key={deliveryOption.id} className="delivery-option">
+          <div key={deliveryOption.id} className="delivery-option"
+          onClick={updateDeliveryOption}>
             <input
               type="radio"
               checked={deliveryOption.id === cartItem.deliveryOptionId}
+              onChange={() => {}}
               className="delivery-option-input"
               name={`delivery-option-${cartItem.productId}`}
             />
