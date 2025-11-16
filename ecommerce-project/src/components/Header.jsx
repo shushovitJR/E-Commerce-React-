cc35b50402fc5cc3a46bbf002cfa4a1e8194c4f2
shushovitJR <ranashushovit@gmail.com>
2025-11-16 19:36:27 +0545

    change logo

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -11,7 +11,7 @@ export function Header({ cart }) {
     <div className="header">
       <div className="left-section">
         <Link to="/" className="header-link">
-          <img className="logo" src="images/logo-white.png" />
+          <img className="logo" src="images/mobile-logo-white.png" />
           <img className="mobile-logo" src="images/mobile-logo-white.png" />
         </Link>
       </div>
